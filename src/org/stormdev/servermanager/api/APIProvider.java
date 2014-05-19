9f3f1f86e0ed664607e8b36faac3c9e5f84ee0e9
storm345 <storm345dev@gmail.com>
2014-05-19 17:27:11 +0100

    Second commit

---
+++ src/org/stormdev/servermanager/api/APIProvider.java
@@ -0,0 +1,19 @@
+package org.stormdev.servermanager.api;
+
+public class APIProvider {
+	private static ServerManagerAPI api = null;
+	
+	private static void setAPI(ServerManagerAPI api){
+		if(api != null){
+			return;
+		}
+		APIProvider.api = api;
+	}
+	
+	/** Returns the API for you to use
+	 * 
+	 */
+	public static ServerManagerAPI getAPI(){
+		return api;
+	}
+}
