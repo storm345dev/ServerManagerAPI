03764d25996e23de0e8a6726a06d2b640ffdae98
storm345 <storm345dev@gmail.com>
2014-05-20 18:55:31 +0100

    Implement message receiving

--- src/org/stormdev/servermanager/api/events/MessageReceiveEvent.java
+++ src/org/stormdev/servermanager/api/events/MessageReceiveEvent.java
@@ -2,14 +2,23 @@ package org.stormdev.servermanager.api.events;
 
 import org.stormdev.servermanager.api.listeners.SMEvent;
 
-/**
- * SMEvent for when a message is received
- *
- */
-public interface MessageReceiveEvent extends SMEvent {
-	/**
-	 * Get the message that was received
-	 * @return The message received
-	 */
-	public ReceivedMessage getMessage();
+public class MessageReceiveEvent implements SMEvent {
+
+	private ReceivedMessage msg;
+	public MessageReceiveEvent(ReceivedMessage msg){
+		this.msg = msg;
+	}
+	
+	public String getEventName() {
+		return "Message Receive Event";
+	}
+
+	public Class<? extends SMEvent> getEventClass() {
+		return MessageReceiveEvent.class;
+	}
+
+	public ReceivedMessage getMessage() {
+		return msg;
+	}
+	
 }
